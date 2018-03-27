15b33d55add166deb295c3b7a78626bebe8c1c58
Lewis Betances <patience174@hotmail.com>
2018-03-27 16:44:14 -0700

    passwordGenerator

--- passwordGen.js
+++ passwordGen.js
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Random Password Generator</title>
-</head>
-<body>
-    
-    <script>
+
     
     
 function randomPasswordGenerator() {
@@ -28,7 +18,4 @@ Math.random().toString(36).substr(2, 5); //password generator
 Math.random().toString(36).substring(2); //password generator   
     
  
-    
-    </script>
-</body>
-</html>
+
